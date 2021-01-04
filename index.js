20ceb90ec849209a77ff8266b2484ed76fc0e30c
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-01-04 22:24:40 +0530

    new: Table with all countries COVID data

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const init = require('./lib/init.js');
 const ora = require('ora');
 const Table = require('cli-table3');
 const fetchWorldwide = require('./lib/worldwide_data.js');
+const fetchCountry = require('./lib/country_data.js');
 const { style, single, colored, borderless } = require('./lib/table.js')
 const legend = require('./lib/legend');
 
+
 const [input] = cli.input;
 const xcolor = cli.flags.xcolor;
+const sortBy = cli.flags.sort;
+const reverse = cli.flags.reverse;
+const limit = Math.abs(cli.flags.limit);
+const chart = cli.flags.chart;
+const log = cli.flags.log;
 const minimal = cli.flags.minimal;
-const json = cli.flags.json;
+const options = { sortBy, limit, reverse, minimal, chart, log };
 unhandled();
 
 (async () => {
@@ -20,12 +27,15 @@ unhandled();
     await init(minimal);
     const spinner = ora({ text: '' });
     input === 'help' && (await cli.showHelp(0));
+    const country = input;
 
     const head = xcolor ? single : colored;
     const border = minimal ? borderless : {};
     const OutputFormat = Table;
     const output = new OutputFormat({ head, style, chars: border });
+
     spinner.start();
-    const lastUpdated = await fetchWorldwide(output, json);
+    const lastUpdated = await fetchWorldwide(output);
+    await fetchCountry(spinner, output, country, options);
     legend(lastUpdated);
 })();
